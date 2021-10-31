15595e257d59e91824f99b22ae56388af4a8a16a
ImrangirHossain <imrangir.920@gmail.com>
2021-10-31 16:13:39 +0600

    update service page added

--- src/Component/Service/Service.js
+++ src/Component/Service/Service.js
@@ -9,7 +9,7 @@ const Service = (props) => {
                     <div className="card  h-100">
                     <img height="225" src={img} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
+                    <h5 className="card-title">{key}.{title}</h5>
                      <p><small> {discription}</small></p>          
                      <NavLink to={`/placeOrder/${key}`}>
                           <button className="btn btn-primary"> BOOK NOW <i className="text-dark fas fa-shopping-cart"></i></button>
